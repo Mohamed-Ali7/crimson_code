b8c6623a56b3b332b285c266446afea85d822b43
Mohamed Ali <mohammed.ali17955@gmail.com>
2025-04-11 15:25:47 +0200

    Preparing the project structure

---
+++ crimson_code_blog_rest_apis/src/main/java/com/crimson_code_blog_rest_apis/CrimsonCodeBlogRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.crimson_code_blog_rest_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsonCodeBlogRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimsonCodeBlogRestApisApplication.class, args);
+	}
+
+}
---
+++ crimson_code_blog_rest_apis/src/test/java/com/crimson_code_blog_rest_apis/CrimsonCodeBlogRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimson_code_blog_rest_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimsonCodeBlogRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
