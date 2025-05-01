767889d4e214785b5db37a0fd465d5795b5c6b14
Mohamed <mohammed.ali17955@gmail.com>
2025-05-01 15:21:39 +0300

    Implement password reset pages

---
+++ web_client/static/scripts/password_reset.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const token = urlParams.get(`token`);
+
+  $.ajax({
+    method: `GET`,
+    url: `http://localhost:8080/api/users/password-reset/validate?token=${token}`,
+    success: function () {
+      $(`.password-reset-card`).show();
+      return;
+    },
+    error: function () {
+      $(`.error-message-container`).show();
+      $(`.requset-new-rest-link-btn`).show();
+    }
+  });
+
+  $(`.requset-new-rest-link-btn`).on(`click`, function () {
+    window.location = `password_reset_request.html`;
+  });
+
+  function showError(inputField, message) {
+    inputField.addClass('error');
+    inputField.closest(`.form-group`).next('.error-message').css(`visibility`, `visible`).text(message);
+  }
+
+  function clearErrors(inputField) {
+    inputField.removeClass(`valid`);
+    inputField.removeClass('error');
+    inputField.closest(`.form-group`).next('.error-message').css(`visibility`, `hidden`);
+  }
+
+  function isNewPasswordValid(newPasswordInput) {
+    clearErrors(newPasswordInput);
+    if (newPasswordInput.val().trim().length < 8) {
+      showError(newPasswordInput, 'Password must be at least 8 characters.');
+      return false;
+    }
+    newPasswordInput.addClass('valid');
+    return true;
+  }
+
+  function isConfirmPasswordValid(confirmPasswordInput) {
+    clearErrors(confirmPasswordInput);
+    if ($('.new-password-input').val().trim() !== confirmPasswordInput.val().trim()) {
+      showError(confirmPasswordInput, 'Passwords do not match.');
+      return false;
+    }
+    if (confirmPasswordInput.val().trim().length === 0) {
+      return false
+    }
+    confirmPasswordInput.addClass('valid');
+    return true;
+  }
+
+  $(`.new-password-input`).on(`blur`, function () {
+    isNewPasswordValid($(this));
+    clearErrors($(`.confirm-password-input`));
+  });
+  
+  $(`.confirm-password-input`).on(`blur`, function () {
+    isConfirmPasswordValid($(this));
+  });
+
+  $('.bi-eye-slash').on('click', function () {
+    const passwordInput = $(this).prev($('input'));
+    if (passwordInput.attr('type') === 'password') {
+      passwordInput.attr('type', 'text');
+    } else {
+      passwordInput.attr('type', 'password');
+    }
+    $(this).toggleClass('bi-eye');
+  });
+
+  $('.password-reset-form').on('submit', (e) => {
+    e.preventDefault();
+
+    const newPasswordInput = $('.new-password-input');
+    const confirmPasswordInput = $(`.confirm-password-input`);
+
+    if (!isNewPasswordValid(newPasswordInput) || !isConfirmPasswordValid(confirmPasswordInput)) {
+      return;
+    }
+    
+    const passwordResetData = {
+      newPassword: newPasswordInput.val().trim(),
+      confirmPassword: confirmPasswordInput.val().trim(),
+      token: token,
+    };
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/api/users/password-reset/confirm",
+      data: JSON.stringify(passwordResetData),
+      contentType: 'application/json',
+      success: (data) => {
+        sessionStorage.setItem('flush_message','Password Reset Successful')
+        window.location = `login.html`;
+      },
+      error: (response) => {
+        if(response.status === 400) {
+          $(`.form-input`).removeClass(`valid`);
+          $(`.new-password-input`).addClass(`error`);
+          showError($(`.confirm-password-input`), response.responseJSON.message);
+        } else {
+          console.error(response.responseJSON.message);
+        }
+      }
+    });
+  });
+});
---
+++ web_client/static/scripts/password_reset_request.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+
+  function showError(inputField, message) {
+    inputField.addClass('error');
+    $('.error-message').css(`visibility`, `visible`).text(message);
+  }
+
+  function clearErrors(inputField) {
+    inputField.removeClass('error');
+    $('.error-message').css(`visibility`, `hidden`);
+  }
+
+  function isEmailValid(emailInput) {
+    clearErrors(emailInput)
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!re.test(emailInput.val().trim())) {
+      showError(emailInput, `Please enter a valid email address`);
+      return false;
+    }
+    return true;
+  }
+
+  $('.password-reset-request-form').on('submit', (e) => {
+    e.preventDefault();
+    const emailInput = $('.email-input');
+
+    if (!isEmailValid(emailInput)) {
+      return;
+    }
+  
+    const userData = {
+      email: emailInput.val().trim(),
+    };
+
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:8080/api/users/password-reset/request",
+      data: JSON.stringify(userData),
+      contentType: 'application/json',
+      success: (data) => {
+        sessionStorage.setItem(
+          'flush_message',
+          'Password reset link has been sent to your email'
+        );
+        window.location = `login.html`
+      },
+      error: (response) => {
+        if (response.status === 404) {
+          showError($(`.email-input`), `This email address doesn't exist.`);
+        } else {
+          alert(`Something went wrong, please try again later or contact support`);
+        }
+      }
+    });
+  });
+});
